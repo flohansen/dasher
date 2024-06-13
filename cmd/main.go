4e2e17e85897885bdcaa8c01f7d426b4261f75a2
Florian Hansen <flhansen.93@gmail.com>
2024-06-13 20:13:09 +0200

    feat: add migration to server

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,47 @@ package main
 
 import (
 	"database/sql"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"path"
 
 	"github.com/flohansen/dasher-server/internal/datastore"
 	"github.com/flohansen/dasher-server/internal/routes"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	"github.com/pkg/errors"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func run() error {
+	dataPath := flag.String("data", "/data", "The full path to the sqlite3 database file")
+	flag.Parse()
+
 	log.Println("listening on port 3000")
-	db, err := sql.Open("sqlite3", "./dasher.db")
+	db, err := sql.Open("sqlite3", path.Join(*dataPath, "dasher.db"))
 	if err != nil {
 		return errors.Wrap(err, "sql open")
+	}
+
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		return errors.Wrap(err, "migrate")
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", path.Join(*dataPath, "migrations")),
+		"sqlite3",
+		driver,
+	)
+	if err != nil {
+		return errors.Wrap(err, "migrate")
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return errors.Wrap(err, "migrate")
 	}
 
 	featureStore := datastore.NewSQLite(db)
